de8cc7b12e0efb6b3dfbd922a14aa63061274309
C. Jeffrey <86826906+cst0313@users.noreply.github.com>
2022-03-04 13:19:42 +0000

    Clear Symmetry

--- 200-299/202C.cpp
+++ 200-299/202C.cpp
@@ -1,10 +1,11 @@
+//202C
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
     double n;
     cin>>n;
-    if(n==3){cout<<5; return 0;}
-    int x=ceil(sqrt(2*n-1));
+    if(n==3){cout<<5; return 0;}//edge case
+    int x=ceil(sqrt(2*n-1));//reader is encouraged to find pattern
     cout<<x+1-x%2;
     return 0;
 }
