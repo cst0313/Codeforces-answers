fa4192faccca916d76431c6e656517df887534d0
C. Jeffrey <86826906+cst0313@users.noreply.github.com>
2022-03-05 04:41:25 +0000

    Flowers

--- 400-499/474D.cpp
+++ 400-499/474D.cpp
@@ -9,7 +9,7 @@ const ll mod=1000000007;
 int main(){
   ll t,k,templ,templ2,m;
   cin>>t>>k;
-  m=-1;
+  m=-1;//size of mem array, could make a fixed one with 10000
   vector<pair<ll,ll>>v;
   for(ll i=0;i<t;i++){
     cin>>templ>>templ2;
@@ -21,29 +21,26 @@ int main(){
   dp[1][0]=1;
   dp[0][0]=1;
   if(k==1){
-    dp[1][1]=1;
+    dp[1][1]=1;//we can put a single W
   }
   for(ll i=2;i<m+1;i++){
-    dp[i][0]=(dp[i-1][0]+dp[i-1][1])%mod;
+    dp[i][0]=(dp[i-1][0]+dp[i-1][1])%mod;//prev R or prev W
     if(i>=k){
-      dp[i][1]=(dp[i-k][0]+dp[i-k][1])%mod;
+      dp[i][1]=(dp[i-k][0]+dp[i-k][1])%mod;//added chunk of W, extract combin from k before
     }
-  }/*
-  for(ll i=0;i<m+1;i++){
-    cout<<dp[i][0]<<" ";
   }
-  cout<<endl;
-  for(ll i=0;i<m+1;i++){
-    cout<<dp[i][1]<<" "; 
-  }
-  cout<<endl;*/
-  ll a[m+1];
+  ll a[m+1];//prefix sum
   a[0]=0;
   for(ll i=1;i<m+1;i++){
-    a[i]=(a[i-1]+dp[i][0]+dp[i][1])%mod;
+    a[i]=(a[i-1]+dp[i][0]+dp[i][1])%mod;//all combinations is sum of those ending with R and Wd
   }
   for(auto x:v){
     cout<<(a[x.second]-a[x.first-1]+mod)%mod<<endl;
   }
   return 0;
 }
+/*Alt solution:
+dp[i]=dp[i-1];
+if(i>=k){dp[i]+=dp[i-k];}
+*/
+
