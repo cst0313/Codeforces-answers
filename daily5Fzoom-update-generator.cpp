b47ec7086be6c5617cc5a4535b3cb8c8cb3c0bdf
C. Jeffrey <86826906+cst0313@users.noreply.github.com>
2022-03-16 15:06:42 +0000

    index.html code generator

---
+++ daily5Fzoom-update-generator.cpp
@@ -0,0 +1,49 @@
+//349C
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+ 
+int main(){
+  string date;
+  getline(cin,date);
+  ll lessons;
+  cin>>lessons;
+  string type;
+  string ts,te;
+  cout<<"           if(date==\""<<date<<"\"){"<<endl;
+  while(lessons--){
+    cout<<"           ";
+    cout<<"  ";
+    cin>>type;
+    getline(cin,ts);
+    getline(cin,ts);
+    getline(cin,te);
+    cout<<"if(time>=\""<<ts<<"\" && time<\""<<te<<"\"){"<<endl;
+    cout<<"               link="<<type<<";"<<endl;
+    cout<<"             }"<<endl;
+  }
+  cout<<'}'<<endl;
+  return 0;
+}//shift option k to align
+/*
+3-24
+math
+8:15
+8:45
+bre
+9:20
+9:35
+physics
+9:35
+9:50
+math
+10:35
+10:55
+break
+10:
+fin
+12:35
+23:59
+*/
