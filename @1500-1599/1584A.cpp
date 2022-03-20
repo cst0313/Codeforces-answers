79d425cf817d3c4e1477231c9a96d0379e54f2a9
C. Jeffrey <86826906+cst0313@users.noreply.github.com>
2022-03-20 03:06:52 +0000

    Mathematical Addition

---
+++ @1500-1599/1584A.cpp
@@ -0,0 +1,16 @@
+//1584A
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+int main(){
+    ll n;
+    cin>>n;
+    ll u,v;
+    for(ll i=0;i<n;i++){
+        cin>>u>>v;
+        cout<<-1*u*u<<" "<<v*v<<endl;//math, try solving on both sides
+    }
+  return 0;
+}
